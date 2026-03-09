bc3e139352a349bd11f29448c9a48e0dc20c9a25
mugilansGIT <23aids065@act.edu.in>
2026-03-09 13:33:22 +0530

    Changes

---
+++ visual.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x=[1,2,3,4,5]
+y=[2,3,5,7,11]
+plt.plot(x,y)
+plt.xlabel('x-axis')    
+plt.ylabel('y-axis')
+plt.title('Line Graph')
+plt.show()
